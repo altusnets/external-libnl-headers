fbc7d22ee3163913f6a73170c1e5022d123281f9
Binesh B <binesh@motorola.com>
2015-08-03 10:29:50 -0500

    ARP check before DHCP
    enable RTNL library for ROUTE and NEIGH operations via nl
    from supplicant
    
    Change-Id: I2e964628531d85c90325267af92271dc19e9029d
    Reviewed-on: http://gerrit.mot.com/779394
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Ranjeet Gupta <ranjeetgupta@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    SLTApproved: Binesh Balasingh <binesh@motorola.com>

--- Android.mk
+++ Android.mk
@@ -39,3 +39,55 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 LOCAL_MODULE := libnl
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES :=  \
+		lib/route/addr.c \
+		lib/route/class.c \
+		lib/route/class_api.c \
+		lib/route/class_obj.c \
+		lib/route/cls.c \
+		lib/route/cls_api.c \
+		lib/route/cls_obj.c \
+		lib/route/link.c \
+		lib/route/neigh.c \
+		lib/route/neightbl.c \
+		lib/route/nexthop.c \
+		lib/route/qdisc.c \
+		lib/route/qdisc_api.c \
+		lib/route/qdisc_obj.c \
+		lib/route/route.c \
+		lib/route/route_obj.c \
+		lib/route/route_utils.c \
+		lib/route/rtnl.c \
+		lib/route/rule.c \
+		lib/route/tc.c \
+		lib/route/cls/fw.c \
+		lib/route/cls/police.c \
+		lib/route/cls/u32.c \
+		lib/route/link/api.c \
+		lib/route/link/vlan.c \
+		lib/route/sch/blackhole.c \
+		lib/route/sch/cbq.c \
+		lib/route/sch/dsmark.c \
+		lib/route/sch/fifo.c \
+		lib/route/sch/htb.c \
+		lib/route/sch/netem.c \
+		lib/route/sch/prio.c \
+		lib/route/sch/red.c \
+		lib/route/sch/sfq.c \
+		lib/route/sch/tbf.c \
+		lib/fib_lookup/lookup.c \
+		lib/fib_lookup/request.c
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_MODULE := libnl_route
+
+include $(BUILD_STATIC_LIBRARY)
+include $(CLEAR_VARS)
+
+LOCAL_WHOLE_STATIC_LIBRARIES := libnl
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
+LOCAL_MODULE := libnl_route
+
+include $(BUILD_SHARED_LIBRARY)
